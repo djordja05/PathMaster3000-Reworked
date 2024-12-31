16f7e5dc95063a23ec38bd113ba5b698578a6ee9
djordja05 <djordjic.filip5@gmail.com>
2024-12-31 16:05:55 +0100

    -v0.2 -Added AppearancesMenu -Added GridSizeMenu

---
+++ src/GridSizeMenu.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GridSizeMenu extends JPanel {
+
+    public GridSizeMenu(PathMaster3000 frame) {
+        setLayout(new GridLayout(20, 1));
+
+        String[] sizes = {"1 x 1", "2 x 2", "3 x 3", "4 x 4", "5 x 5", "6 x 6", "7 x 7", "8 x 8", "9 x 9", "10 x 10", "11 x 11", "12 x 12"};
+        JComboBox<String> gridSizes = new JComboBox<>(sizes);
+        gridSizes.setSelectedIndex(0);
+        add(gridSizes);
+
+        JButton setGridSize = new JButton("Set");
+        setGridSize.addActionListener(e -> frame.setGridSize(Integer.parseInt(String.valueOf(gridSizes.getSelectedItem().toString().charAt(0)))));
+        add(setGridSize);
+
+        JButton back = new JButton("Back");
+        back.addActionListener(e -> frame.showOptionsMenu());
+        add(back);
+    }
+
+}
--- src/OptionsMenu.java
+++ src/OptionsMenu.java
@@ -6,17 +6,15 @@ public class OptionsMenu extends JPanel {
     public OptionsMenu(PathMaster3000 frame) {
         setLayout(new GridLayout(20, 1));
 
-
-        JButton appereance = new JButton("Appereance");
-        appereance.setVisible(true);
-        add(appereance);
+        JButton appearance = new JButton("Appereance");
+        appearance.addActionListener(e -> frame.showAppearancesMenu());
+        add(appearance);
 
         JButton gridSize =  new JButton("Grid Size");
-        gridSize.setVisible(true);
+        gridSize.addActionListener(e -> frame.showGridSizeMenu());
         add(gridSize);
 
         JButton back = new JButton("Back");
-        back.setVisible(true);
         back.addActionListener(e -> frame.showMainMenu());
         add(back);
 
